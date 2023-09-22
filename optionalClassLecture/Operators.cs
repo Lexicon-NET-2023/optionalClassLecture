f95a13707de53f41a2761712810d0a2e46ecc3e0
Jonathan Krall <jonathan.krall@lexicon.se>
2023-09-22 12:04:49 +0200

    Add project files.

---
+++ optionalClassLecture/Operators.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace optionalClassLecture
+{
+    internal class Operators
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine($"{a} + {b} = {a+b}");
+            Console.ReadKey(); 
+        }
+
+        internal void Divide(int a, int b)
+        {
+            if (b != 0)
+            {
+                Console.WriteLine($"{a} / {b} = {a / b}");
+            }
+            else
+            {
+                throw new DivideByZeroException(); 
+            }
+            Console.ReadKey();
+        }
+
+        internal void Multiply(int a, int b)
+        {
+            Console.WriteLine($"{a} * {b} = {a * b}");
+            Console.ReadKey();
+        }
+
+        internal void Subract(int a, int b)
+        {
+            Console.WriteLine($"{a} - {b} = {a - b}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ optionalClassLecture/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.ComponentModel;
+
+namespace optionalClassLecture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Operators op = new Operators(); 
+            bool isAlive = true;
+            while (isAlive)
+            {
+                try
+                {
+                    Console.Clear();
+                    Console.WriteLine("Welcome to Krallculator 3000! \n" +
+                        "Please select your numbers followed by one of the following operators: \n" +
+                        "+: Add\n" +
+                        "-: Subtract\n" +
+                        "*: Multiply\n" +
+                        "/: Divide\n" +
+                        "Q: Quit");
+
+                    Console.Write("Please choose your first number: ");
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Please choose your second number: ");
+                    int b = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Select your operator (+ - * /): ");
+                    ConsoleKey selection = Console.ReadKey().Key;
+
+                    Console.WriteLine();
+
+                    switch (selection)
+                    {
+                        case ConsoleKey.Add:
+                            op.Add(a, b);
+                            break;
+                        case ConsoleKey.Subtract:
+                            op.Subract(a, b);
+                            break;
+                        case ConsoleKey.Multiply:
+                            op.Multiply(a, b);
+                            break;
+                        case ConsoleKey.Divide:
+                            op.Divide(a, b);
+                            break;
+                        case ConsoleKey.Q:
+                            Console.WriteLine("Thank you for using my Krallculator 3000!");
+                            isAlive = false;
+                            //Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("Not a valid choice");
+                            Console.ReadKey();
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.ReadKey();
+                }
+                //catch(DivideByZeroException)
+                //{
+                //    throw; 
+                //}
+               
+            }
+
+        }
+    }
+}
